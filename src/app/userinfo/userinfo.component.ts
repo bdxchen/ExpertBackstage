47b76516dc072dd5ef5ea2ed0e9c60c3f677cdc1
八千米海岸 <529454926@qq.com>
2018-02-01 15:08:57 +0800

    用户信息模块

--- src/app/userinfo/userinfo.component.ts
+++ src/app/userinfo/userinfo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Headers, Http } from '@angular/http';
+import "rxjs/Rx";
 
 @Component({
   selector: 'app-userinfo',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserinfoComponent implements OnInit {
 
-  constructor() { }
+  dataSource:Observable<any>;
+
+  stocks = []
+
+
+  constructor(public http: Http) {
+    let myHeaders:Headers = new Headers();
+    myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiNTEzNjIzIiwicm9sZSI6IlVzZXIiLCJleHAiOjE1NDgxMjc4NzEsInZlciI6MH0=.ewByvyaVymmuhSsbW7Ns/6DZ+Zr6Cs71ouGtAaauBRU=")
+    this.dataSource = this.http.get('https://api.duoniuapp.com/v1/User/GetUserInfo', {headers: myHeaders}).map(response => response.json());
+  }
 
   ngOnInit() {
+    this.dataSource.subscribe(
+      data => console.log(data)
+    )
+    console.log(this.dataSource)
   }
 
 }
